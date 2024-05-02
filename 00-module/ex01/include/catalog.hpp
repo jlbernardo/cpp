188c46a400c57ea3c151ad2c688eb7fba6bbceb6
Juliany <julianybernardo@outlook.com>
2024-05-02 15:24:43 -0300

    (chore) Header banners for clearer comprehension of unit blocks

--- 00-module/ex01/include/catalog.hpp
+++ 00-module/ex01/include/catalog.hpp
@@ -6,25 +6,38 @@
 /*   By: julberna <julberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 01:47:54 by julberna          #+#    #+#             */
-/*   Updated: 2024/05/02 01:52:55 by julberna         ###   ########.fr       */
+/*   Updated: 2024/05/02 15:20:47 by julberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once //includes this header once
 #ifndef CATALOG_HPP
 # define CATALOG_HPP
 
-/* Libraries */
+
+// ************************************************************************** //
+//                                 Libraries                                  //
+// ************************************************************************** //
+
 # include <cstring>
 # include <iostream>
 # include "Contact.hpp"
 # include "PhoneBook.hpp"
 
-/* Readability macros */
+
+// ************************************************************************** //
+//                              Readabily Macros                              //
+// ************************************************************************** //
+
 # define EMPTY_CNT	1
 # define WRONG_CMD	2
 # define INDEX_OUT	3
 
-/* Main program */
+
+// ************************************************************************** //
+//                           Main Program Functions                           //
+// ************************************************************************** //
+
 void			goodbye(void);
 void			error(int flag);
 void			prompt(std::string &input, std::string msg);
