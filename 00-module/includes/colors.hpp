3ce20c84ae0e50bf92d45d969bcf2b6e5c767f9c
Juliany <julianybernardo@outlook.com>
2024-05-05 23:19:00 -0300

    (refactor) Remove attempt of spacing from colors.hpp

--- 00-module/includes/colors.hpp
+++ 00-module/includes/colors.hpp
@@ -6,7 +6,7 @@
 /*   By: julberna <julberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 18:47:52 by julberna          #+#    #+#             */
-/*   Updated: 2024/05/02 18:51:57 by julberna         ###   ########.fr       */
+/*   Updated: 2024/05/05 22:37:16 by julberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,49 +16,41 @@
 
 /**
  * Default color.
- * #
  */
 # define DFL	"\033[0m"
 
 /**
  * Red bold color.
- * #
  */
 # define RED	"\033[31;1m"
 
 /**
  * Green bold color.
- * #
  */
 # define GRN	"\033[32;1m"
 
 /**
  * Yellow bold color.
- * #
  */
 # define YLW	"\033[33;1m"
 
 /**
  * Blue bold color.
- * #
  */
 # define BLU	"\033[34;1m"
 
 /**
  * Purple bold color.
- * #
  */
 # define PRP	"\033[35;1m"
 
 /**
  * Cyan bold color.
- * #
  */
 # define CYN	"\033[36;1m"
 
 /**
  * White bold color.
- * #
  */
 # define WHT	"\033[37;1m"
 
--- 01-module/includes/colors.hpp
+++ 01-module/includes/colors.hpp
@@ -6,7 +6,7 @@
 /*   By: julberna <julberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 18:47:52 by julberna          #+#    #+#             */
-/*   Updated: 2024/05/02 18:51:57 by julberna         ###   ########.fr       */
+/*   Updated: 2024/05/05 22:52:46 by julberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,53 +14,52 @@
 #ifndef COLORS_HPP
 # define COLORS_HPP
 
+# include <cstdlib>
+
 /**
  * Default color.
- * #
  */
 # define DFL	"\033[0m"
 
 /**
  * Red bold color.
- * #
  */
 # define RED	"\033[31;1m"
 
 /**
  * Green bold color.
- * #
  */
 # define GRN	"\033[32;1m"
 
 /**
  * Yellow bold color.
- * #
  */
 # define YLW	"\033[33;1m"
 
 /**
  * Blue bold color.
- * #
  */
 # define BLU	"\033[34;1m"
 
 /**
  * Purple bold color.
- * #
  */
 # define PRP	"\033[35;1m"
 
 /**
  * Cyan bold color.
- * #
  */
 # define CYN	"\033[36;1m"
 
 /**
  * White bold color.
- * #
  */
 # define WHT	"\033[37;1m"
+
+/**
+ * Inserts content into the std::cout and adds a new line at the end.
+ */
+# define println(x) std::cout << x << std::endl;
 
 
 #endif
