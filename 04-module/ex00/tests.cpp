2a9d0d3f7b041ccb5f43719f7da0a13aea2f2705
Juliany <julianybernardo@outlook.com>
2024-06-05 21:29:22 -0300

    (chore) Improves lore writting

--- 04-module/ex00/tests.cpp
+++ 04-module/ex00/tests.cpp
@@ -6,7 +6,7 @@
 /*   By: julberna <julberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 20:23:41 by julberna          #+#    #+#             */
-/*   Updated: 2024/06/05 21:17:16 by julberna         ###   ########.fr       */
+/*   Updated: 2024/06/05 21:28:22 by julberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,8 @@ void	testDogClass(void) {
 
 void	testWrongAnimalClass(void) {
 
-	println("  One day, however, resulting from a weird experiment,\n  Animal mutated and gave origin to... Something weird.")
+	println("  One day, however, resulting from an unconventional experiment,"
+			"\n  Animal mutated and gave origin to... Something weird.")
 		const WrongAnimal* weirdo = new WrongAnimal();
 
 	println("\n  It looked like Animal, but would make funny sounds...");
@@ -77,10 +78,11 @@ void	testWrongCatClass(void) {
 	println("  The funniest thing was when this other creature came up.")
 		const WrongAnimal* wrongo = new WrongCat();
 
-	println("\n  Its type was \"" << wrongo->getType() << "\", but looked wrong and would sound even wronger.");
+	println("\n  Its type was \"" << wrongo->getType() <<
+			"\", but looked wrong and would sound even wronger.");
 		wrongo->makeSound();
 
-	println("\n  Luckily, it was put out of its misery in no time.")
+	println("\n  Luckily, it was put out of its misery before it caused too much trouble.")
 		delete wrongo;
 }
 
