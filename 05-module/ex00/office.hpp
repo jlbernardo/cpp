8f2668c8f2c7f7d19d0a835f52e6e8e4070e79f2
Juliany <julianybernardo@outlook.com>
2024-06-13 13:55:24 -0300

    (docs) Documentation for test header

--- 05-module/ex00/office.hpp
+++ 05-module/ex00/office.hpp
@@ -6,7 +6,7 @@
 /*   By: julberna <julberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 21:35:28 by julberna          #+#    #+#             */
-/*   Updated: 2024/06/13 12:24:09 by julberna         ###   ########.fr       */
+/*   Updated: 2024/06/13 13:54:45 by julberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,29 @@
 
 # include "Bureaucrat.hpp"
 
+		/**
+		 * @brief Tests the creation of a Bureaucrat.
+		 */
 void	testBureaucratCreation(void);
+
+		/**
+		 * @brief Tests the creation of a Bureaucrat with a grade too high.
+		 */
 void	testGradeTooHighException(void);
+
+		/**
+		 * @brief Tests the creation of a Bureaucrat with a grade too low.
+		 */
 void	testGradeTooLowException(void);
+
+		/**
+		 * @brief Tests the incrementation of a Bureaucrat's grade.
+		 */
 void	testIncrementGrade(void);
+
+		/**
+		 * @brief Tests the decrementation of a Bureaucrat's grade.
+		 */
 void	testDecrementGrade(void);
 
 #endif
