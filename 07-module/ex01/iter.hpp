e22b8e6dfdeab86a29692668a7d241c0aede420c
Juliany Bernardo <julianybernardo@outlook.com>
2024-07-03 03:27:04 -0300

    (refactor) Frufru

--- 07-module/ex01/iter.hpp
+++ 07-module/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: julberna <julberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 00:05:52 by julberna          #+#    #+#             */
-/*   Updated: 2024/07/03 00:39:22 by julberna         ###   ########.fr       */
+/*   Updated: 2024/07/03 03:22:06 by julberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,13 @@ void iter(T *array, int len, void (*f)(T &)) {
 
 template<typename T>
 void print(T const &i) {
-	println(i);
+	std::cout << i << " " << std::flush;
 }
 
 template<typename T>
 void plusFive(T &i) {
 	i = i + 5;
-	println(static_cast<T>(i));
+	print(i);
 }
 
 #endif
--- 07-module/ex01/main.cpp
+++ 07-module/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: julberna <julberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 00:06:41 by julberna          #+#    #+#             */
-/*   Updated: 2024/07/03 00:40:59 by julberna         ###   ########.fr       */
+/*   Updated: 2024/07/03 03:26:37 by julberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,31 @@
 
 int main(void){
 	int tab[] = {0, 1, 2, 3, 4};
+
+	println(WHT << "\n We can iterate over an array of any type and apply any function to it:");
+	std::cout << PRP << "   ";
 	::iter(tab, 5, print);
+	std::cout << RED;
 	::iter(tab, 5, plusFive<int>);
 
-	println("");
+	println("\n");
 
+
+	println(WHT << " It can be a char array:");
+	std::cout << PRP << "   ";
 	char teb[] = {'a', 'b', 'c', 'd', 'e'};
 	::iter(teb, 5, print);
+	std::cout << RED;
 	::iter(teb, 5, plusFive<char>);
 
-	println("");
+	println("\n");
 
+	println(WHT << " Or any other custom type we create, like the Fixed points from another list:");
+	std::cout << PRP << "   ";
 	Fixed tib[] = {Fixed(1.1f), Fixed(2.2f), Fixed(3.3f), Fixed(4.4f), Fixed(5.55f)};
 	::iter(tib, 5, print);
+	std::cout << RED;
 	::iter(tib, 5, plusFive<Fixed>);
+	println("");
 }
 
